495706223b0375bc08349ab0d53a47f178b7c44f
fariz0704 <55374829+fariz0704@users.noreply.github.com>
2020-01-10 13:11:58 +0500

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -1,3 +1,22 @@
 package main
 
+import "testing"
 
+func Test_distance(t *testing.T) {
+  tests := []struct {
+    name         string
+    fuelConsumed int
+    amountOfFuel int
+    want         int
+  }{
+    {"More than hundred km", 7, 14, 200},
+    {"Exactly hundred km", 9, 9, 100},
+    {"Less than hundred km", 10, 1, 10},
+  }
+  for _, test := range tests {
+    got := distance(test.fuelConsumed, test.amountOfFuel)
+    if got != test.want {
+      t.Error("Test fail:", test.name, "got:", got, "want:", test.want)
+    }
+  }
+}
