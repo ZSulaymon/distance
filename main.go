a8c6cc6e112dee06334efb023ac2e9e6878b2b25
fariz0704 <55374829+fariz0704@users.noreply.github.com>
2020-01-10 13:10:24 +0500

    Update main.go

--- main.go
+++ main.go
@@ -3,17 +3,8 @@ package main
 func main() {
 
 }
-func bonus(sales []int) int {
-	const (
-		percentBound = 5
-		percent      = 100
-		saleBound    = 10_000
-	)
-	sumBonus := 0
-	for _, sale := range sales{
-		if sale > saleBound {
-			sumBonus += (sale - saleBound) * percentBound / percent
-		}
-	}
-	return sumBonus
+func distance(fuelConsumed int, amountOfFuel int) int {
+  const distanceInKm int = 100
+  carCanRideFor := distanceInKm * amountOfFuel / fuelConsumed
+  return carCanRideFor
 }
